f06435b02bac58390731a61eccff42775b1e3ff6
hangw2130 <hangw2130@gmail.com>
2024-11-18 11:09:58 +0900

    spring boot backend

--- project/src/main/java/com/hangw/model/Restaurant.java
+++ project/src/main/java/com/hangw/model/Restaurant.java
@@ -13,6 +13,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -20,15 +21,19 @@ import lombok.Setter;
 @Setter
 @Entity
 @Component
+@Table(name="restaurant_table")
 public class Restaurant {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long Id;
 	
+	@Column(name = "business_name")
 	private String name;
 	
+	@Column(name = "location_address")
 	private String address;
 	
+	@Column(name = "road_address")
 	private String roadAdress;
 	
 	@Column(nullable = true)
@@ -37,11 +42,19 @@ public class Restaurant {
 	@Column(nullable = true)
     private double longitude;
     
+	@Column(name = "business_type")
 	private String category;
 	
 	@ColumnDefault("0")
     private double score;
     
+	@Column(name = "location_phone")
+	private String phone;
+	
+	private String locationZipcode;
+	
+	private String roadZipcode;
+	
     @OneToMany(mappedBy = "restaurant", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
 	private List<Review> Reviews;
 }
