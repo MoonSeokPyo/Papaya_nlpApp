60f9f0cbd6f3131364f2eed6508d2da521900c40
SeokPyo Moon <happy1es@naver.com>
2024-12-06 13:49:29 +0900

    Project reset #23

--- project/src/main/java/com/hangw/model/DataNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.hangw.model;
-
-public class DataNotFoundException extends RuntimeException{
-	public DataNotFoundException() {}
-	public DataNotFoundException(String message) {
-		super(message);
-	}
-}
